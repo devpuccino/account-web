4311cdb12bd414545fa2cbe1b36d7b20dcceb813
Janepop Atirattanachai <jane3030@gmail.com>
2024-09-07 09:14:01 +0700

    add header category

--- src/app/category/layout.tsx
+++ src/app/category/layout.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 export default function CategoryLayout({ children, }: { children: React.ReactNode }) {
     return (
         <div>
-            <h1 id="page-header">Category</h1>
+            
             {children}
         </div>
     )
--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,42 +1,44 @@
 export default async function CategoryPage() {
-    // const categoryList: Category[] = await CategoryService.getAllCategory()
     const categoryList = [
         {
             id: '1',
-            category_name:'Food' ,
+            category_name: 'Food',
             active: true
         },
         {
             id: '2',
-            category_name:'Coffee and Tea',
+            category_name: 'Coffee and Tea',
             active: true
         },
         {
             id: '3',
-            category_name:'Transport',
+            category_name: 'Transport',
             active: true
         }
     ]
     return (
-        <table id="category-data-table" className="data-table">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th className="text-left">Category Name</th>
-                    <th className="text-left">Active Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    categoryList?.map((category, index) => {
-                        return <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{category.category_name}</td>
-                            <td>{category.active ? "ACTIVE" : "INACTIVE"}</td>
-                        </tr>
-                    })
-                }
-            </tbody>
-        </table>
+        <div>
+            <h1 id="page-header">Category</h1>
+            <table id="category-data-table" className="data-table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th className="text-left">Category Name</th>
+                        <th className="text-left">Active Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        categoryList?.map((category, index) => {
+                            return <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{category.category_name}</td>
+                                <td>{category.active ? "ACTIVE" : "INACTIVE"}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
