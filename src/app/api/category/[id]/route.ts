31151cc66c02d825ef332fcb4eca778f6bea0fff
Janepop Atirattanachai <jane3030@gmail.com>
2024-10-01 20:34:38 +0700

    add get category by id api

--- src/app/api/category/[id]/route.ts
+++ src/app/api/category/[id]/route.ts
@@ -1,13 +1,24 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export const GET = async(request:NextRequest, context:{params:{ id:string }}) =>{
-    let data = { id: '1', category_name: 'Food', active: true }
-    let responseBody = {
-        status: "200-000",
-        message: "Success",
-        data: data
+    let responseBody = null
+    let httpStatus = 200
+    try{
+        let result = await fetch (`http://localhost:8080/account-service/api/category/${context.params.id}`)
+        let {code,message,data} = await result.json()
+        responseBody = {
+            status: code,
+            message: message,
+            data: data
+        }
+    }catch(error){
+        responseBody = {
+            status: "500-001",
+            message: "Unexpected error"
+        }
+        httpStatus = 500
     }
-    return NextResponse.json(responseBody)
+    return NextResponse.json(responseBody,{status : httpStatus})
 }
 
 export const PUT = async(request:NextRequest,context:{params:{id:string}})=>{
