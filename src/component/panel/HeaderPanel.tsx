22077df25a08f4399007f032f23294d03e2d1488
Janepop Atirattanachai <jane3030@gmail.com>
2024-11-23 23:29:03 +0700

    add ant-design and initial root layout

---
+++ src/component/panel/HeaderPanel.tsx
@@ -0,0 +1,33 @@
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons"
+import { Button, Menu } from "antd"
+import { Header } from "antd/es/layout/layout"
+import styled from "styled-components"
+const HeaderContainer = styled(Header)`
+    padding: 0;
+`
+const CollapseButton = styled(Button)`
+    font-size: 16px;
+    width: 64px;
+    height: 64px;
+`
+interface HeaderPanelProperties {
+    sideBarCollapsed: boolean
+    setSidebarCollapsed: Function
+    bgThemeColor: string
+}
+const HeaderPanel = ({ sideBarCollapsed, setSidebarCollapsed, bgThemeColor }: HeaderPanelProperties) => {
+    const doOnCollapseClick = ()=>{
+        setSidebarCollapsed(!sideBarCollapsed)
+    }
+    const displayCollapseIcon = ()=>{
+        if(sideBarCollapsed){
+            return <MenuUnfoldOutlined />
+        }else{
+            return <MenuFoldOutlined />
+        }
+    }
+    return <HeaderContainer style={{ background: bgThemeColor }}>
+        <CollapseButton type="text" icon={displayCollapseIcon()} onClick={doOnCollapseClick} />
+    </HeaderContainer>
+}
+export default HeaderPanel
---
+++ src/component/panel/SidebarPanel.tsx
@@ -0,0 +1,31 @@
+import { AccountBookOutlined } from "@ant-design/icons"
+import { Menu } from "antd"
+import Sider from "antd/es/layout/Sider"
+import { ItemType } from "antd/es/menu/interface"
+import Link from "next/link"
+import styled from "styled-components"
+const LogoPanel = styled.div`
+    height: 64px;
+    padding: 5px 5px 5px 20px;
+    line-height: 64px;
+    text-transform: uppercase;
+    
+`
+interface SidebarPanelProperties {
+    sideBarCollapsed: boolean
+    bgThemeColor: string
+}
+const SidebarPanel = ({ sideBarCollapsed, bgThemeColor }: SidebarPanelProperties) => {
+    const menuItems:ItemType[] = [
+        { key: "1", icon: <AccountBookOutlined />, label: <Link href="/wallet">Wallet</Link> }
+    ]
+    return <Sider trigger={null} collapsed={sideBarCollapsed} collapsible={true} style={{ background: bgThemeColor }}>
+        <LogoPanel>
+            account web
+        </LogoPanel>
+        <Menu 
+            items={menuItems}
+        />
+    </Sider>
+}
+export default SidebarPanel
